ef25237193aeaa20fde0621b6714462b18349cd5
t3rmin41 <t33rmin41@gmail.com>
2018-04-05 15:41:17 +0300

    Security skeleton added

--- src/main/java/com/minimal/eshop/security/UserNotFoundException.java
+++ src/main/java/com/minimal/eshop/security/UserNotFoundException.java
@@ -2,4 +2,14 @@ package com.minimal.eshop.security;
 
 public class UserNotFoundException extends Exception {
 
+  private String message;
+  
+  public UserNotFoundException(String message) {
+    this.message = message;
+  }
+
+  public String getMessage() {
+    return message;
+  }
+
 }
