27806f7fe3b932159cf64dcb1229c559df1fd830
t3rmin41 <t33rmin41@gmail.com>
2018-04-12 15:05:38 +0300

    Skeleton

--- src/main/java/com/minimal/eshop/view/controller/IndexController.java
+++ src/main/java/com/minimal/eshop/view/controller/IndexController.java
@@ -1,8 +1,15 @@
 package com.minimal.eshop.view.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class IndexController {
 
+  @RequestMapping(value="/", method = RequestMethod.GET)
+  public String index() {
+    return "index";
+  }
+  
 }
