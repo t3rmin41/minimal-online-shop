96b59a844a411c03a4048660d482d662fd105644
t3rmin41 <t33rmin41@gmail.com>
2018-04-15 22:18:39 +0300

    @Transactional

--- src/main/java/com/minimal/eshop/repository/ProductRepositoryImpl.java
+++ src/main/java/com/minimal/eshop/repository/ProductRepositoryImpl.java
@@ -1,37 +1,48 @@
 package com.minimal.eshop.repository;
 
 import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
 import org.springframework.stereotype.Repository;
 import com.minimal.eshop.jpa.Product;
 
 @Repository
 public class ProductRepositoryImpl implements ProductRepository {
 
+  @PersistenceContext
+  private EntityManager em;
+  
   @Override
+  @Transactional
   public List<Product> getAllProducts() {
     // TODO Auto-generated method stub
     return null;
   }
 
   @Override
+  @Transactional
   public Product getProductById(Long id) {
     // TODO Auto-generated method stub
     return null;
   }
 
   @Override
+  @Transactional
   public Product saveProduct(Product jpa) {
     // TODO Auto-generated method stub
     return null;
   }
 
   @Override
+  @Transactional
   public Product updateProduct(Product jpa) {
     // TODO Auto-generated method stub
     return null;
   }
 
   @Override
+  @Transactional
   public boolean deleteProductById(Long id) {
     // TODO Auto-generated method stub
     return false;
