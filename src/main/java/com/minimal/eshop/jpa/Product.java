8450afb96c376f5f94a5b68d88189ab5f69ff27a
t3rmin41 <t33rmin41@gmail.com>
2018-04-16 13:46:56 +0300

    JPA classes

--- src/main/java/com/minimal/eshop/jpa/Product.java
+++ src/main/java/com/minimal/eshop/jpa/Product.java
@@ -1,5 +1,50 @@
 package com.minimal.eshop.jpa;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "PRODUCT")
 public class Product {
+
+  @Id
+  @GeneratedValue(strategy=GenerationType.AUTO)
+  @Column(name = "ID")
+  private Long id;
+  @Column(name = "TITLE")
+  private String title;
+  @Column(name = "PRICE")
+  private Double price;
+  @Column(name = "DELETED")
+  private boolean deleted = false;
+
+  public Long getId() {
+    return id;
+  }
+  public void setId(Long id) {
+    this.id = id;
+  }
+  public String getTitle() {
+    return title;
+  }
+  public void setTitle(String title) {
+    this.title = title;
+  }
+  public Double getPrice() {
+    return price;
+  }
+  public void setPrice(Double price) {
+    this.price = price;
+  }
+  public boolean isDeleted() {
+    return deleted;
+  }
+  public void setDeleted(boolean deleted) {
+    this.deleted = deleted;
+  }
 
 }
--- src/main/java/com/minimal/eshop/jpa/Role.java
+++ src/main/java/com/minimal/eshop/jpa/Role.java
@@ -18,47 +18,37 @@ public class Role {
   @GeneratedValue(strategy = GenerationType.AUTO)
   @Column(name = "ID")
   private Long id;
-
   @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "USER_ID", nullable = false)
   private User user;
-
   @Column(name = "ROLE")
   private String role;
-
   @Column(name = "ACTIVE")
   private Long active = 1L;
 
   public Long getId() {
     return id;
   }
-
   public void setId(Long id) {
     this.id = id;
   }
-
   public User getUser() {
     return user;
   }
-
   public void setUser(User user) {
     this.user = user;
   }
-
   public String getRole() {
     return role;
   }
-
   public void setRole(String role) {
     this.role = role;
   }
-
   public Long getActive() {
     return active;
   }
-
   public void setActive(Long active) {
     this.active = active;
   }
-  
+
 }
