b3be410a6b80cf31f5340d7414a6b32286258217
t3rmin41 <t33rmin41@gmail.com>
2018-04-25 15:09:48 +0300

    Modified for traditional .war deployment

--- src/main/java/com/minimal/eshop/app/ShopApplication.java
+++ src/main/java/com/minimal/eshop/app/ShopApplication.java
@@ -4,6 +4,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Import;
 import com.minimal.eshop.config.ApplicationConfig;
@@ -15,15 +17,15 @@ import com.minimal.eshop.security.SecurityConfig;
 
 @SpringBootApplication
 @Import({ApplicationConfig.class, MvcConfig.class, JpaConfig.class, SwaggerConfig.class, SecurityConfig.class}) // H2DatabaseStop.class})
-public class ShopApplication { // extends SpringBootServletInitializer {
+public class ShopApplication extends SpringBootServletInitializer {
   
   private static Logger log = LoggerFactory.getLogger(ShopApplication.class);
 
   //for traditional .war deployment need to extend SpringBootServletInitializer
-  //@Override
-  //protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-  //    return application.sources(ShopApplication.class);
-  //}
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+      return application.sources(ShopApplication.class);
+  }
 
   public static void main(String[] args) {
     SpringApplication springApplication = new SpringApplication(ShopApplication.class);
