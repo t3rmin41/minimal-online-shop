8258319e29ee777171fdedf649fed73c77d6af6b
t3rmin41 <t33rmin41@gmail.com>
2018-04-25 15:26:13 +0300

    JPA config modified

--- src/main/java/com/minimal/eshop/config/JpaConfig.java
+++ src/main/java/com/minimal/eshop/config/JpaConfig.java
@@ -24,18 +24,19 @@ public class JpaConfig {
   @Bean(name = "dataSource")
   public DriverManagerDataSource dataSource() {
     DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
-    driverManagerDataSource.setDriverClassName("org.h2.Driver");
-    driverManagerDataSource.setUsername("sa");
     if ("prod".equals(activeProfile)) {
       driverManagerDataSource.setDriverClassName("org.postgresql.Driver");
       driverManagerDataSource.setUrl("jdbc:postgresql://ec2-54-75-244-248.eu-west-1.compute.amazonaws.com:5432/def9rmt7j8882a");
       driverManagerDataSource.setUsername("tbjznubkniaknu"); // using account credentials
       driverManagerDataSource.setPassword("4b95dfb88aa6f03b3c374e74b64dff988848be8a2e41fd6a9787438fdf14e2c1");
     } else if ("test".equals(activeProfile)) {
+      driverManagerDataSource.setDriverClassName("org.h2.Driver");
       driverManagerDataSource.setUrl("jdbc:h2:./db/test/bin;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=TRUE");
       driverManagerDataSource.setPassword("t3sts3cr3t");
     } else if ("dev".equals(activeProfile)) {
+      driverManagerDataSource.setDriverClassName("org.h2.Driver");
       driverManagerDataSource.setUrl("jdbc:h2:./db/dev/bin;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=TRUE");
+      driverManagerDataSource.setUsername("sa");
       //driverManagerDataSource.setPassword("d3vs3cr3t");
     }
     return driverManagerDataSource;
